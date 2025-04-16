abba279ed322c470bc1dd2db7b441aee63916a54
IvanGutierrez <e.ivangutierrez@go.ugr.es>
2025-04-16 14:29:30 +0200

    Update Chatter.ino

--- Chatter/Chatter.ino
+++ Chatter/Chatter.ino
@@ -1,9 +1,22 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <ESP32Time>
+#include <LoRa.h">
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Screen parameters and pins
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//LoRa module parameters and pins
+
+//Screen parameters and pins
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
